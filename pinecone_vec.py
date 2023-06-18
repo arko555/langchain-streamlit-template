cc13365a38ce8a1be70f91642fd259ab1de17744
Arkodip Nag <nagarkodip14@gmail.com>
2023-06-18 21:30:49 +0530

    "attempted pinecone vector embeddding"

--- pinecone_vec.py
+++ pinecone_vec.py
@@ -5,3 +5,21 @@ import pinecone
 import os
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+my_loader = DirectoryLoader('./research_papers', glob='**/*.pdf')
+documents = my_loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 700, chunk_overlap = 0)
+docs = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+pinecone.init(
+    api_key=os.environ['PINECONE_API_KEY'],  # find at app.pinecone.io
+    environment=os.environ['PINECONE_ENV']  # next to api key in console
+)
+
+docsearch = Pinecone.from_documents(docs, embeddings, index_name=os.environ['PINECONE_INDEX_NAME'])
+
+query = "what is attention mechanism"
+docs = docsearch.similarity_search(query)
+print(docs[0].page_content)
